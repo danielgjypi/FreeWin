8e7c1860429f8bb46fc585ae4d0bfe13bc8e877a
theBadDaniel <78989432+theBadDaniel@users.noreply.github.com>
2021-11-07 14:51:46 +0100

    Better user experience
    Overall changes to guide the user

--- app/main.bat
+++ app/main.bat
@@ -3,7 +3,7 @@ color 02
 echo Welcome to theBadDaniel's Windows 10 Activator
 echo Please make sure you run the app as an Admininstrator
 echo ------------------------------------------------------
-echo [S]tart [H]elp [A]bout
+echo [S]tart [H]elp [A]bout [C]heck windows version
 set /p input=
 if %input%==s (
 start runner.bat
@@ -18,5 +18,11 @@ exit
 if %input%==a (
 start about.txt
 start info.vbs
+exit
+)
+if %input%==c (
+start C:\Windows\System32\winver.exe
+pause
+start main.bat
 exit
 )
--- app/runner.bat
+++ app/runner.bat
@@ -1,7 +1,21 @@
 @echo off
 color 02
-echo ------------------------------------------------------
+echo --------------------------------------
 echo Please select your Windows 10 Edition:
+echo --------------------------------------
+echo Windows 10 Pro - pro
+echo Windows 10 Pro N - pron
+echo Windows 10 Home - home
+echo Windows 10 Home N - homen
+echo Windows 10 Home Single Language - homes
+echo Windows 10 Home Country Specific - homecs
+echo Windows 10 Education - edu
+echo Windows 10 Education N - edun
+echo Windows 10 Enterprise - ent
+echo Windows 10 Enterprise N - entn
+echo ----------------------------------------------------------------------------
+echo Type one of the prefixes mentioned above (according to your windows edition)
+echo ----------------------------------------------------------------------------
 set /P id=
 start %id%.lnk
 exit
